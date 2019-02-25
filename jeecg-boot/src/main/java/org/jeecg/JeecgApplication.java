28c2ddcc944c1ef72eb10d533c8154584e733834
zhangdaihao <zhangdaiscott@163.com>
2019-02-25 17:14:07 +0800

    jeecg-boot 1.0 版本发布

--- jeecg-boot/src/main/java/org/jeecg/JeecgApplication.java
+++ jeecg-boot/src/main/java/org/jeecg/JeecgApplication.java
@@ -1,13 +1,10 @@
 package org.jeecg;
 
-import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class JeecgApplication {
 
     public static void main(String[] args) {
