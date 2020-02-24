3ece8f17d233153ce18a031209e4d258bd588e1b
刘慧斌 <651044782@qq.com>
2020-02-24 16:08:25 +0800

    '11'

--- ant-design-vue-jeecg/src/views/dashboard/Analysis.vue
+++ ant-design-vue-jeecg/src/views/dashboard/Analysis.vue
@@ -1,41 +1,45 @@
 <template>
-  <!--<div>-->
-    <!--<index-chart v-if="indexStyle==1"></index-chart>-->
-    <!--<index-bdc v-if="indexStyle==2"></index-bdc>-->
-    <!--<index-task v-if="indexStyle==3"></index-task>-->
-    <!--<div style="width: 100%;text-align: right;margin-top: 20px">-->
-      <!--请选择首页样式：-->
-      <!--<a-radio-group v-model="indexStyle">-->
-        <!--<a-radio :value="1">统计图表</a-radio>-->
-        <!--<a-radio :value="2">统计图表2</a-radio>-->
-        <!--<a-radio :value="3">任务表格</a-radio>-->
-      <!--</a-radio-group>-->
-    <!--</div>-->
-  <!--</div>-->
+  <div>
+    <index-sy v-if="indexStyle==0"></index-sy>
+    <index-chart v-if="indexStyle==1"></index-chart>
+    <index-bdc v-if="indexStyle==2"></index-bdc>
+    <index-task v-if="indexStyle==3"></index-task>
+    <div style="width: 100%;text-align: right;margin-top: 20px">
+      请选择首页样式：
+      <a-radio-group v-model="indexStyle">
+        <a-radio :value="0">首页</a-radio>
+        <a-radio :value="1">统计图表</a-radio>
+        <a-radio :value="2">统计图表2</a-radio>
+        <a-radio :value="3">任务表格</a-radio>
+      </a-radio-group>
+    </div>
+  </div>
 </template>
 
 <script>
-  import IndexChart from './IndexChart'
-  import IndexTask from "./IndexTask"
-  import IndexBdc from './IndexBdc'
+import IndexSy from './IndexSy'
+import IndexChart from './IndexChart'
+import IndexTask from "./IndexTask"
+import IndexBdc from './IndexBdc'
 
-  export default {
-    name: "Analysis",
-    components: {
-      IndexChart,
-      IndexTask,
-      IndexBdc
-    },
-    data() {
-      return {
-        indexStyle:2
-      }
-    },
-    created() {
-
-    },
-    methods: {
-
+export default {
+  name: "Analysis",
+  components: {
+    IndexSy,
+    IndexChart,
+    IndexTask,
+    IndexBdc
+  },
+  data () {
+    return {
+      indexStyle: 0
     }
+  },
+  created () {
+
+  },
+  methods: {
+
   }
+}
 </script>
---
+++ ant-design-vue-jeecg/src/views/dashboard/IndexSy.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    1111
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  methods: {},
+  mounted() {}
+}
+</script>
+
+<style>
+
+</style>
