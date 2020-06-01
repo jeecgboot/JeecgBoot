7c10b3d1d52d09bc8ad31faacd2464db0f173c90
yunpeng4456 <1441331786@qq.com>
2020-06-01 10:53:55 +0800

    企业资质 优化

--- ant-design-vue-jeecg/src/views/business/company/oneCompayOneRecord/routeView/Qualification.vue
+++ ant-design-vue-jeecg/src/views/business/company/oneCompayOneRecord/routeView/Qualification.vue
@@ -2,22 +2,25 @@
   <div>
     <a-tabs default-active-key="1"  >
       <a-tab-pane key="1" tab="企业形象">
-        <img style="width: 100%;" :src="controlCrafts"  :preview="controlCrafts">
+        <img style="width: 100%;" :src="companyImage"  :preview="controlCrafts">
       </a-tab-pane>
       <a-tab-pane key="2" tab="营业执照照片" force-render>
         <img style="width: 100%;" :src="businessLicense"  :preview="businessLicense">
 
       </a-tab-pane>
       <a-tab-pane key="3" tab="企业平面图">
-        Content of Tab Pane 3
+        <img style="width: 100%;" :src="floorPlan"  :preview="businessLicense">
+
       </a-tab-pane>
 
       <a-tab-pane key="4" tab="生产工艺图">
-        Content of Tab Pane 3
+        <img style="width: 100%;" :src="produceCrafts"  :preview="businessLicense">
+
       </a-tab-pane>
       <a-tab-pane key="5" tab="治理工艺图">
-      Content of Tab Pane 3
-    </a-tab-pane>
+        <img style="width: 100%;" :src="controlCrafts"  :preview="businessLicense">
+
+      </a-tab-pane>
 
     </a-tabs>
 
@@ -28,6 +31,8 @@
     //一企一档 企业资质
     //企业形象照片
     import {loadQualifications} from "../../../requestAction/request"
+    import {getFileAccessHttpUrl} from '@/api/manage';
+
     export default {
       name: "Qualification",
       props:{
@@ -53,13 +58,12 @@
         let that = this;
         //查询
         loadQualifications({companyId:that.companyId}).then((res)=>{
-          debugger
           if(res.success){
-            that.companyImage=res.result.companyImage;
-            that.controlCrafts=res.result.controlCrafts;
-            that.businessLicense=res.result.businessLicense;
-            that.floorPlan=res.result.floorPlan;
-            that.produceCrafts=res.result.produceCrafts;
+            that.companyImage=getFileAccessHttpUrl(res.result.companyImage);
+            that.controlCrafts=getFileAccessHttpUrl(res.result.controlCrafts);
+            that.businessLicense=getFileAccessHttpUrl(res.result.businessLicense);
+            that.floorPlan=getFileAccessHttpUrl(res.result.floorPlan);
+            that.produceCrafts=getFileAccessHttpUrl(res.result.produceCrafts);
           }else{
             console.log(res.message);
           }
