31689c640c3bbd18c47a7b384a7009bcd1b0ad79
yunpeng4456 <1441331786@qq.com>
2020-06-04 20:03:51 +0800

    常量数据

--- jeecg-boot/jeecg-boot-module-company/src/main/java/org/jeecg/modules/business/utils/Constant.java
+++ jeecg-boot/jeecg-boot-module-company/src/main/java/org/jeecg/modules/business/utils/Constant.java
@@ -3,8 +3,17 @@ package org.jeecg.modules.business.utils;
 public class Constant {
     //状态
     public  static class status{
-
+        /**
+         * 正常 正在生效装填
+         */
         public static String NORMAL = "NORMAL";
-
+        /**
+         * 过期  无效的状态--历史数据
+         */
+        public static String EXPIRED = "EXPIRED";
+        /**
+         * 待审核 刚提交的状态
+         */
+        public static String PEND = "PEND";
     }
 }
