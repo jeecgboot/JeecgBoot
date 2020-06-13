32ee9f040c0020dc7131fdf652e4b3ce8831b393
wcdsdy <1152039581@qq.com>
2020-06-13 17:30:38 +0800

    修复路由传参的值传送到jinput框被前后各截取了一位

--- ant-design-vue-jeecg/src/components/jeecg/JInput.vue
+++ ant-design-vue-jeecg/src/components/jeecg/JInput.vue
@@ -56,7 +56,10 @@
           let text = this.value
           switch (this.type) {
             case JINPUT_QUERY_LIKE:
-              text = text.substring(1,text.length-1);
+            //修复路由传参的值传送到jinput框被前后各截取了一位
+            if(text.indexOf("*") != -1){
+                text = text.substring(1,text.length-1);
+              }
               break;
             case JINPUT_QUERY_NE:
               text = text.substring(1);
