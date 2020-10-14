d35e21451eae8ee14bdc4c75158d4d34881e080e
song chao <dlhtsc@126.com>
2020-10-14 15:05:03 +0800

    增加hello测试页面

---
+++ ant-design-vue-jeecg/src/views/jeecg/hello.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+  import {getAction} from '@/api/manage'
+  export default {
+    data () {
+      return {
+        msg: ""
+      }
+    },
+    methods: {
+      hello () {
+        var url = "/test/jeecgDemo/hello"
+        getAction(url).then((res) => {
+          if (res.success) {
+            this.msg = res.result;
+          }
+        })
+      }
+    },
+    created() {
+      this.hello();
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
