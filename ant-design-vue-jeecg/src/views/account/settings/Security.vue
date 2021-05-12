e49e76ca7adfca5c00edfde8ba29adf971da8255
Andrew Zhang <andrewzhang1996@hotmail.com>
2021-05-12 17:25:37 +0800

    Update Security.vue
    增加用户账户设置-安全设置修改密码功能

--- ant-design-vue-jeecg/src/views/account/settings/Security.vue
+++ ant-design-vue-jeecg/src/views/account/settings/Security.vue
@@ -1,4 +1,5 @@
 <template>
+<div>
   <a-list
     itemLayout="horizontal"
     :dataSource="data"
@@ -18,20 +19,40 @@
 
     </a-list-item>
   </a-list>
+
+  <user-password ref="userPassword"></user-password>
+</div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+import UserPassword from '../../../components/tools/UserPassword'
   export default {
     data () {
       return {
         data: [
-          { title: '账户密码' , description: '当前密码强度', value: '强', actions: { title: '修改', callback: () => { this.$message.info('This is a normal message'); } } },
+          /* update_begin author:andrewzhang date:20210512 for: 账户设置-安全设置修改密码 */
+          // { title: '账户密码' , description: '当前密码强度', value: '强', actions: { title: '修改', callback: () => { this.$message.info('This is a normal message'); } } },
+          { title: '账户密码' , description: '当前密码强度', value: '强', actions: { title: '修改', callback: () => { this.updatePassword(); } } },
+          /* update_end author:andrewzhang date:20210512 for: 账户设置-安全设置修改密码 */
           { title: '密保手机' , description: '已绑定手机', value: '138****8293', actions: { title: '修改', callback: () => { this.$message.success('This is a message of success'); } }  },
           { title: '密保问题' , description: '未设置密保问题，密保问题可有效保护账户安全', value: '', actions: { title: '设置', callback: () => { this.$message.error('This is a message of error'); } }  },
           { title: '备用邮箱' , description: '已绑定邮箱', value: 'ant***sign.com', actions: { title: '修改', callback: () => { this.$message.warning('This is message of warning'); } }  },
           { title: 'MFA 设备' , description: '未绑定 MFA 设备，绑定后，可以进行二次确认', value: '', actions: { title: '绑定', callback: () => { this.$message.info('This is a normal message'); } }  },
         ]
       }
+    },
+    components:{
+      UserPassword,
+    },
+    methods: {
+      /* update_begin author:andrewzhang date:20210512 for: 账户设置-安全设置修改密码 */
+      ...mapGetters(["userInfo"]),
+      updatePassword(){
+        let username = this.userInfo().username
+        this.$refs.userPassword.show(username)
+      }
+      /* update_end author:andrewzhang date:20210512 for: 账户设置-安全设置修改密码 */
     }
   }
 </script>
