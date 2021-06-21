1beddbf8e8ef6ce4ebc35e7b184aa3fd6f072e32
zhangdaiscott <zhangdaiscott@163.com>
2021-06-21 19:29:21 +0800

    online表单数据源配置，不支持数据库密码加密 #2672

--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/config/init/CodeGenerateDbConfig.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/config/init/CodeGenerateDbConfig.java
@@ -1,5 +1,6 @@
 package org.jeecg.config.init;
 
+import com.alibaba.druid.filter.config.ConfigTools;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.jeecgframework.codegenerate.database.CodegenDatasourceConfig;
@@ -25,11 +26,21 @@ public class CodeGenerateDbConfig {
     private String password;
     @Value("${spring.datasource.dynamic.datasource.master.driver-class-name:}")
     private String driverClassName;
+    @Value("${spring.datasource.dynamic.datasource.master.druid.public-key:}")
+    private String publicKey;
 
 
     @Bean
     public CodeGenerateDbConfig initCodeGenerateDbConfig() {
         if(StringUtils.isNotBlank(url)){
+            if(StringUtils.isNotBlank(publicKey)){
+                try {
+                    password = ConfigTools.decrypt(publicKey, password);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    log.error(" 代码生成器数据库连接，数据库密码解密失败！");
+                }
+            }
             CodegenDatasourceConfig.initDbConfig(driverClassName,url, username, password);
             log.info(" 代码生成器数据库连接，使用application.yml的DB配置 ###################");
         }
