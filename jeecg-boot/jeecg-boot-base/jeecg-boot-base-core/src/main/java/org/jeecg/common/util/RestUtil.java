28bbd72dc04125ca9a571f879d2e15f584f7aac8
zhangdaiscott <zhangdaiscott@163.com>
2021-08-31 22:28:28 +0800

    issues/2959 微服务版集成企业微信单点登录

--- jeecg-boot/jeecg-boot-base/jeecg-boot-base-core/src/main/java/org/jeecg/common/util/RestUtil.java
+++ jeecg-boot/jeecg-boot-base/jeecg-boot-base-core/src/main/java/org/jeecg/common/util/RestUtil.java
@@ -25,6 +25,12 @@ public class RestUtil {
     public static String getDomain() {
         if (domain == null) {
             domain = SpringContextUtils.getDomain();
+            // issues/2959
+            // 微服务版集成企业微信单点登录
+            // 因为微服务版没有端口号，导致 SpringContextUtils.getDomain() 方法获取的域名的端口号变成了:-1所以出问题了，只需要把这个-1给去掉就可以了。
+            if (domain.endsWith(":-1")) {
+                domain = domain.substring(0, domain.length() - 3);
+            }
         }
         return domain;
     }
