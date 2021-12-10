07b19492dd229e23d9dbfca576bc08f5b1e4e14e
zhangdaiscott <zhangdaiscott@163.com>
2021-12-10 11:19:29 +0800

    【#3225】常见案例-自定义组件-cron表达式显示错误

--- ant-design-vue-jeecg/src/components/jeecg/JEasyCron/tabs/const.js
+++ ant-design-vue-jeecg/src/components/jeecg/JEasyCron/tabs/const.js
@@ -1,11 +1,11 @@
 export const WEEK_MAP_EN = {
-  'SUN': '0',
-  'MON': '1',
-  'TUE': '2',
-  'WED': '3',
-  'THU': '4',
-  'FRI': '5',
-  'SAT': '6'
+  'SUN': '1',
+  'MON': '2',
+  'TUE': '3',
+  'WED': '4',
+  'THU': '5',
+  'FRI': '6',
+  'SAT': '7'
 }
 
 export const replaceWeekName = (c) => {
@@ -14,7 +14,7 @@ export const replaceWeekName = (c) => {
     Object.keys(WEEK_MAP_EN).forEach(k => {
       c = c.replace(new RegExp(k, 'g'), WEEK_MAP_EN[k])
     })
-    c = c.replace(new RegExp('7', 'g'), '0')
+    // c = c.replace(new RegExp('7', 'g'), '0')
   }
   // console.info('after: ' + c)
   return c
