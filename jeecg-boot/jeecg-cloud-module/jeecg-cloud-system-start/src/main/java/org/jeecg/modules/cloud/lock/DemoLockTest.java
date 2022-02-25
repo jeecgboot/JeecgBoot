51c63e805714ffd5d00b78724b546d45cc7b1792
zhangdaiscott <zhangdaiscott@163.com>
2022-02-25 22:19:41 +0800

    Nacos服务地址配置,默认用host jeecg-boot-nacos 默认注释掉mq测试示例代码，减少入门难度

--- jeecg-boot/jeecg-cloud-module/jeecg-cloud-system-start/src/main/java/org/jeecg/modules/cloud/lock/DemoLockTest.java
+++ jeecg-boot/jeecg-cloud-module/jeecg-cloud-system-start/src/main/java/org/jeecg/modules/cloud/lock/DemoLockTest.java
@@ -1,16 +1,10 @@
 package org.jeecg.modules.cloud.lock;
 
 import lombok.extern.slf4j.Slf4j;
-import org.jeecg.boot.starter.lock.annotation.JLock;
 import org.jeecg.boot.starter.lock.client.RedissonLockClient;
-import org.jeecg.boot.starter.rabbitmq.client.RabbitMqClient;
-import org.jeecg.common.base.BaseMap;
 import org.jeecg.modules.cloud.constant.CloudConstant;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import java.util.Map;
 
 /**
  * 分布式锁测试demo
@@ -20,26 +14,26 @@ import java.util.Map;
 public class DemoLockTest {
     @Autowired
     RedissonLockClient redissonLock;
-    @Autowired
-    RabbitMqClient rabbitMqClient;
+//    @Autowired
+//    RabbitMqClient rabbitMqClient;
 
-    /**
-     * 测试分布式锁【注解方式】
-     */
-    @Scheduled(cron = "0/5 * * * * ?")
-    @JLock(lockKey = CloudConstant.REDISSON_DEMO_LOCK_KEY1)
-    public void execute() throws InterruptedException {
-        log.info("执行execute任务开始，休眠三秒");
-        Thread.sleep(3000);
-        System.out.println("=======================业务逻辑1=============================");
-        Map map = new BaseMap();
-        map.put("orderId", "BJ0001");
-        rabbitMqClient.sendMessage(CloudConstant.MQ_JEECG_PLACE_ORDER, map);
-        //延迟10秒发送
-        map.put("orderId", "NJ0002");
-        rabbitMqClient.sendMessage(CloudConstant.MQ_JEECG_PLACE_ORDER, map, 10000);
-        log.info("execute任务结束，休眠三秒");
-    }
+//    /**
+//     * 测试分布式锁【注解方式】
+//     */
+//    @Scheduled(cron = "0/5 * * * * ?")
+//    @JLock(lockKey = CloudConstant.REDISSON_DEMO_LOCK_KEY1)
+//    public void execute() throws InterruptedException {
+//        log.info("执行execute任务开始，休眠三秒");
+//        Thread.sleep(3000);
+//        System.out.println("=======================业务逻辑1=============================");
+//        Map map = new BaseMap();
+//        map.put("orderId", "BJ0001");
+//        rabbitMqClient.sendMessage(CloudConstant.MQ_JEECG_PLACE_ORDER, map);
+//        //延迟10秒发送
+//        map.put("orderId", "NJ0002");
+//        rabbitMqClient.sendMessage(CloudConstant.MQ_JEECG_PLACE_ORDER, map, 10000);
+//        log.info("execute任务结束，休眠三秒");
+//    }
 
     public DemoLockTest() {
     }
