aae16f51e062b53772d726b4fe5ea541209580a0
zhangdaiscott <zhangdaiscott@163.com>
2022-02-25 22:19:41 +0800

    Nacos服务地址配置,默认用host jeecg-boot-nacos 默认注释掉mq测试示例代码，减少入门难度

--- jeecg-boot/jeecg-cloud-module/jeecg-cloud-system-start/src/main/java/org/jeecg/modules/cloud/feign/controller/JeecgTestFeignController.java
+++ jeecg-boot/jeecg-cloud-module/jeecg-cloud-system-start/src/main/java/org/jeecg/modules/cloud/feign/controller/JeecgTestFeignController.java
@@ -1,12 +1,9 @@
 package org.jeecg.modules.cloud.feign.controller;
 
 
-import cn.hutool.core.util.RandomUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.jeecg.boot.starter.rabbitmq.client.RabbitMqClient;
 import org.jeecg.common.api.vo.Result;
-import org.jeecg.common.base.BaseMap;
 import org.jeecg.modules.cloud.constant.CloudConstant;
 import org.jeecg.modules.cloud.feign.feign.JeecgTestClient;
 import org.jeecg.modules.cloud.feign.feign.JeecgTestClientDyn;
@@ -15,8 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/sys/test")
@@ -27,8 +22,8 @@ public class JeecgTestFeignController {
     private JeecgFeignService jeecgFeignService;
     @Autowired
     private JeecgTestClient jeecgTestClient;
-    @Autowired
-    private RabbitMqClient rabbitMqClient;
+//    @Autowired
+//    private RabbitMqClient rabbitMqClient;
 
     @GetMapping("getMessage")
     @ApiOperation(value = "测试feign", notes = "测试feign")
@@ -43,19 +38,19 @@ public class JeecgTestFeignController {
         return myClientDyn.getMessage("动态fegin——jeecg-boot2");
     }
 
-    @GetMapping(value = "/rabbitmq")
-    @ApiOperation(value = "测试rabbitmq", notes = "测试rabbitmq")
-    public Result<?> rabbitMqClientTest(HttpServletRequest req) {
-        //rabbitmq消息队列测试
-        BaseMap map = new BaseMap();
-        map.put("orderId", RandomUtil.randomNumbers(10));
-        rabbitMqClient.sendMessage(CloudConstant.MQ_JEECG_PLACE_ORDER, map);
-        rabbitMqClient.sendMessage(CloudConstant.MQ_JEECG_PLACE_ORDER_TIME, map,10);
-
-        //rabbitmq消息总线测试
-        BaseMap params = new BaseMap();
-        params.put("orderId", "123456");
-        rabbitMqClient.publishEvent(CloudConstant.MQ_DEMO_BUS_EVENT, params);
-        return Result.OK("MQ发送消息成功");
-    }
+//    @GetMapping(value = "/rabbitmq")
+//    @ApiOperation(value = "测试rabbitmq", notes = "测试rabbitmq")
+//    public Result<?> rabbitMqClientTest(HttpServletRequest req) {
+//        //rabbitmq消息队列测试
+//        BaseMap map = new BaseMap();
+//        map.put("orderId", RandomUtil.randomNumbers(10));
+//        rabbitMqClient.sendMessage(CloudConstant.MQ_JEECG_PLACE_ORDER, map);
+//        rabbitMqClient.sendMessage(CloudConstant.MQ_JEECG_PLACE_ORDER_TIME, map,10);
+//
+//        //rabbitmq消息总线测试
+//        BaseMap params = new BaseMap();
+//        params.put("orderId", "123456");
+//        rabbitMqClient.publishEvent(CloudConstant.MQ_DEMO_BUS_EVENT, params);
+//        return Result.OK("MQ发送消息成功");
+//    }
 }
