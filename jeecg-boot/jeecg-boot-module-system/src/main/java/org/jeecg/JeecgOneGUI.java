1a9200dc7da89b231b8d782cc9aa8f7a6457c748
zhangdaiscott <zhangdaiscott@163.com>
2022-03-18 19:54:18 +0800

    GUI代码生成器适用于vue3

--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/JeecgOneGUI.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/JeecgOneGUI.java
@@ -4,14 +4,13 @@ import org.jeecgframework.codegenerate.window.CodeWindow;
 
 /**
  * @Title: 单表代码生成器入口
- * 【 GUI模式已弃用，请转移Online模式进行代码生成 】
  * @Author 张代浩
  * @site www.jeecg.com
  * @Version:V1.0.1
  */
 public class JeecgOneGUI {
 
-	/** 详细使用手册： http://doc.jeecg.com/2043919 */
+	/** 使用手册： http://doc.jeecg.com/2684691 */
     public static void main(String[] args) {
     	new CodeWindow().pack();
 	}
