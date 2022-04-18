a3d6e0ce08baed91c3121930c18d64290ff00eeb
zhangdaiscott <zhangdaiscott@163.com>
2022-04-18 11:10:38 +0800

    git大小写不敏感，导致重构类名未提交成功

---
+++ jeecg-boot/jeecg-boot-base/jeecg-boot-base-core/src/main/java/org/jeecg/common/util/Md5Util.java
@@ -0,0 +1,47 @@
+package org.jeecg.common.util;
+
+import java.security.MessageDigest;
+
+/**
+ * @Description: 加密工具
+ * @author: jeecg-boot
+ */
+public class Md5Util {
+
+    private static final String[] HEXDIGITS = { "0", "1", "2", "3", "4", "5",
+            "6", "7", "8", "9", "a", "b", "c", "d", "e", "f" };
+
+	public static String byteArrayToHexString(byte[] b) {
+		StringBuffer resultSb = new StringBuffer();
+		for (int i = 0; i < b.length; i++){
+			resultSb.append(byteToHexString(b[i]));
+		}
+		return resultSb.toString();
+	}
+
+	private static String byteToHexString(byte b) {
+		int n = b;
+		if (n < 0) {
+			n += 256;
+		}
+		int d1 = n / 16;
+		int d2 = n % 16;
+		return HEXDIGITS[d1] + HEXDIGITS[d2];
+	}
+
+	public static String md5Encode(String origin, String charsetname) {
+		String resultString = null;
+		try {
+			resultString = new String(origin);
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			if (charsetname == null || "".equals(charsetname)) {
+				resultString = byteArrayToHexString(md.digest(resultString.getBytes()));
+			} else {
+				resultString = byteArrayToHexString(md.digest(resultString.getBytes(charsetname)));
+			}
+		} catch (Exception exception) {
+		}
+		return resultString;
+	}
+
+}
