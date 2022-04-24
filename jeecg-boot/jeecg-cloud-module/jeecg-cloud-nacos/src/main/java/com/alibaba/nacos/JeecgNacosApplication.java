e89423c666e0ec472d926fe76ef94e3738fb8044
zhangdaiscott <zhangdaiscott@163.com>
2022-04-24 17:49:34 +0800

    微服务优化

--- jeecg-boot/jeecg-cloud-module/jeecg-cloud-nacos/src/main/java/com/alibaba/nacos/JeecgNacosApplication.java
+++ jeecg-boot/jeecg-cloud-module/jeecg-cloud-nacos/src/main/java/com/alibaba/nacos/JeecgNacosApplication.java
@@ -1,10 +1,13 @@
 package com.alibaba.nacos;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Nacos 启动类
@@ -33,5 +36,17 @@ public class JeecgNacosApplication {
         //自定义启动端口号
         System.setProperty("server.port","8848");
         SpringApplication.run(JeecgNacosApplication.class, args);
+    }
+
+    /**
+     * 默认跳转首页
+     *
+     * @param model
+     * @return
+     */
+    @GetMapping("/")
+    public String index(Model model, HttpServletResponse response) {
+        // 视图重定向 - 跳转
+        return "/nacos";
     }
 }
