5b108d9ec69a732730b4e27db512965c3d911368
Gauthier LO <lo.gauthier.glo@gmail.com>
2023-07-06 16:13:27 +0200

    fix : archiving job now works as intended, by archiving by default all orders and parcel infos before today 1 year ago

--- jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/business/service/IPlatformOrderService.java
+++ jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/business/service/IPlatformOrderService.java
@@ -145,17 +145,20 @@ public interface IPlatformOrderService extends IService<PlatformOrder> {
     /**
      * Fetch all platform orders between 2 dates and of status erp_status 4 or 5
      * this list will then be archived
-<<<<<<< HEAD
      * @param startDate Start date time
      * @param endDate End date time
-=======
-     *
-     * @param startDate
-     * @param endDate
->>>>>>> 9c5c7432b6824185b589760c500b696f46f82c58
      * @return List of PlatformOrder
      */
-    List<PlatformOrder> fetchPlatformOrdersToArchive(String startDate, String endDate);
+    List<PlatformOrder> fetchOrdersToArchiveBetweenDate(String startDate, String endDate);
+
+    /**
+     * Fetch all platform orders before endDate and of status erp_status 4 or 5
+     * this list will then be archived
+     *
+     * @param endDate Start date time
+     * @return List of PlatformOrder
+     */
+    List<PlatformOrder> fetchOrdersToArchiveBeforeDate(String endDate);
 
     /**
      * Archive a list of platform orders
