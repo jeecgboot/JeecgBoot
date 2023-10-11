e15335ddaba3768d6b1933794c6b81f197887986
zhangdaiscott <zhangdaiscott@163.com>
2023-10-11 10:58:51 +0800

    更新公司名称

--- jeecg-boot-base-core/src/main/java/org/jeecg/config/Swagger2Config.java
+++ jeecg-boot-base-core/src/main/java/org/jeecg/config/Swagger2Config.java
@@ -116,7 +116,7 @@ public class Swagger2Config implements WebMvcConfigurer {
                 // 描述
                 .description("后台API接口")
                 // 作者
-                .contact(new Contact("北京敲敲云科技有限公司","www.jeccg.com","jeecgos@163.com"))
+                .contact(new Contact("北京国炬信息技术有限公司","www.jeccg.com","jeecgos@163.com"))
                 .license("The Apache License, Version 2.0")
                 .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
                 .build();
