02400387aa46c94db9284e78aced24cae282fc13
kezhijie <kezhijie@wuhandsj.com>
2023-12-21 09:46:52 +0800

    Update WechatVerifyController.java

--- jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/system/controller/WechatVerifyController.java
+++ jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/system/controller/WechatVerifyController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
 
 /**
