d44945f6885a3ba4b1d860281eaf84f13a987a70
JEECG <445654970@qq.com>
2024-06-20 19:34:15 +0800

    提示语修改

--- jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/system/controller/CommonController.java
+++ jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/system/controller/CommonController.java
@@ -51,7 +51,7 @@ public class CommonController {
      */
     @GetMapping("/403")
     public Result<?> noauth()  {
-        return Result.error("没有权限，请管理员授权后，刷新用户缓存或退出重新登录!");
+        return Result.error("没有权限，请联系管理员授权，后刷新缓存!");
     }
 
     /**
