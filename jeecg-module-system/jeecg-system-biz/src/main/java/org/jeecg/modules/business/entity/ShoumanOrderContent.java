983d86580e17202349a71f17c0308653917e973b
Qiuyi LI <qiuyi.li@gmail.com>
2024-08-01 17:34:10 +0200

    Adapt ShoumanOrderContent to the fact that now multiple regexes can exist

--- jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/business/entity/ShoumanOrderContent.java
+++ jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/business/entity/ShoumanOrderContent.java
@@ -3,6 +3,7 @@ package org.jeecg.modules.business.entity;
 import lombok.Data;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Data
 public class ShoumanOrderContent {
@@ -19,34 +20,12 @@ public class ShoumanOrderContent {
     private Integer quantity;
     private String productName;
     private String remark;
-    private String contentRecRegex;
-    private String contentExtRegex;
     private BigDecimal price;
     private String imageUrl;
     private String sku;
+    private List<ShoumanRegex> regexList;
 
-    public ShoumanOrderContent(String shopErpCode, String platformOrderId, String platformOrderNumber, String postcode,
-                               String recipient, String city, String country, String platformOrderContentId,
-                               String customizationData, Integer quantity, String productName, String remark,
-                               String contentRecRegex, String contentExtRegex, BigDecimal price, String imageUrl,
-                               String sku) {
-        this.shopErpCode = shopErpCode;
-        this.platformOrderId = platformOrderId;
-        this.platformOrderNumber = platformOrderNumber;
-        this.postcode = postcode;
-        this.recipient = recipient;
-        this.city = city;
-        this.country = country;
-        this.platformOrderContentId = platformOrderContentId;
-        this.customizationData = customizationData;
-        this.quantity = quantity;
-        this.productName = productName;
-        this.remark = remark;
-        this.contentRecRegex = contentRecRegex;
-        this.contentExtRegex = contentExtRegex;
-        this.price = price;
-        this.imageUrl = imageUrl;
-        this.sku = sku;
+    public ShoumanOrderContent() {
     }
 
     @Override
@@ -64,11 +43,10 @@ public class ShoumanOrderContent {
                 ", quantity=" + quantity +
                 ", productName='" + productName + '\'' +
                 ", remark='" + remark + '\'' +
-                ", contentRecRegex='" + contentRecRegex + '\'' +
-                ", contentExtRegex='" + contentExtRegex + '\'' +
                 ", price=" + price +
                 ", imageUrl='" + imageUrl + '\'' +
                 ", sku='" + sku + '\'' +
+                ", regexList='" + regexList + '\'' +
                 '}';
     }
 }
