118775cf79c530e34eaa9b00c1a1d35e923e0f08
JEECG <445654970@qq.com>
2025-07-25 13:14:11 +0800

    【swagger问题】不带/doc.html访问后台项目swaager，接口测试实际请求后台接口地址少了项目前缀

--- jeecg-boot/jeecg-module-system/jeecg-system-start/src/main/java/org/jeecg/JeecgSystemApplication.java
+++ jeecg-boot/jeecg-module-system/jeecg-system-start/src/main/java/org/jeecg/JeecgSystemApplication.java
@@ -40,7 +40,7 @@ public class JeecgSystemApplication extends SpringBootServletInitializer {
         String path = oConvertUtils.getString(env.getProperty("server.servlet.context-path"));
         log.info("\n----------------------------------------------------------\n\t" +
                 "Application Jeecg-Boot is running! Access URLs:\n\t" +
-                "Local: \t\thttp://localhost:" + port + path + "/doc.html\n\t" +
+                "Local: \t\thttp://localhost:" + port + path + "\n\t" +
                 "External: \thttp://" + ip + ":" + port + path + "/doc.html\n\t" +
                 "Swagger文档: \thttp://" + ip + ":" + port + path + "/doc.html\n" +
                 "----------------------------------------------------------");
