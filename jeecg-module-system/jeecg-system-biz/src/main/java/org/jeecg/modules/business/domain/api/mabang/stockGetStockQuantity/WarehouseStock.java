f4016e412cdd67f4d3df1e46fdcc25e788c192ef
Qiuyi LI <qiuyi.li@gmail.com>
2025-08-01 15:11:05 +0200

    Update WarehouseStock model

--- jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/business/domain/api/mabang/stockGetStockQuantity/WarehouseStock.java
+++ jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/business/domain/api/mabang/stockGetStockQuantity/WarehouseStock.java
@@ -5,31 +5,53 @@ import lombok.Data;
 
 @Data
 public class WarehouseStock {
-    /**仓库编号**/
+    /**
+     * 仓库编号
+     **/
     @JSONField(name = "warehouseId")
     private String warehouseId;
-    /**仓库名称**/
+    /**
+     * 仓库名称
+     **/
     @JSONField(name = "warehouseName")
     private String warehouseName;
-    /**库存总数**/
+    /**
+     * 库存总数
+     **/
     @JSONField(name = "stockQuantity")
     private Integer stockQuantity;
-    /**未发货数量 qty in platform orders**/
+    /**
+     * 未发货数量 qty in platform orders
+     **/
     @JSONField(name = "waitingQuantity")
     private Integer waitingQuantity;
-    /**调拨在途**/
+    /**
+     * 调拨在途
+     **/
     @JSONField(name = "allotShippingQuantity")
     private Integer allotShippingQuantity;
-    /**采购在途数量 qty in mabang purchase orders**/
+    /**
+     * 采购在途数量 qty in mabang purchase orders
+     **/
     @JSONField(name = "shippingQuantity")
     private Integer shippingQuantity;
-    /**加工在途量**/
+    /**
+     * 加工在途量
+     **/
     @JSONField(name = "processingQuantity")
     private Integer processingQuantity;
-    /**fba未发货量**/
+    /**
+     * fba未发货量
+     **/
     @JSONField(name = "fbaWaitingQuantity")
     private Integer fbaWaitingQuantity;
-    /**仓位**/
+    /**
+     * 可用库存量
+     **/
+    private Integer availableStockQuantity;
+    /**
+     * 仓位
+     **/
     @JSONField(name = "gridCode")
     private String gridCode;
 }
