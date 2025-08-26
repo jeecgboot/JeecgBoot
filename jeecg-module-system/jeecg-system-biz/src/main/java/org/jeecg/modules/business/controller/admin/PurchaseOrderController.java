2a818fd15db3423b07069d3da1186b9b19b31732
Xue YANG <xuey648@gmail.com>
2025-08-26 15:03:12 +0200

    feat/payment-proof-upload-1-2-7

--- jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/business/controller/admin/PurchaseOrderController.java
+++ jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/business/controller/admin/PurchaseOrderController.java
@@ -250,7 +250,7 @@ public class PurchaseOrderController {
         }
         // TODO : refacto using Reponses
         PurchaseOrder purchaseOrder = new PurchaseOrder();
-        BeanUtils.copyProperties(purchaseOrderPage, purchaseOrder, "paidAmount");
+        BeanUtils.copyProperties(purchaseOrderPage, purchaseOrder,  "totalAmount","finalAmount","discountAmount","paidAmount");
         purchaseOrder.setPaymentDocumentString(new String(purchaseOrderPage.getPaymentDocument()));
         purchaseOrder.setInventoryDocumentString(new String(purchaseOrderPage.getInventoryDocument()));
         Map<String, Responses> responsesMappedByReason = new HashMap<>();
